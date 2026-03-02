669d42016016158ed12f66e12d1356c9f90cad00
Felipe Alves Amodio <felipeaamodio@gmail.com>
2026-03-02 20:07:34 -0300

    Initial Commit

---
+++ BMI Calculator App/BMI_Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_Calculator_AppApp.swift
+//  BMI Calculator App
+//
+//  Created by Felipe Alves Amodio on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct BMI_Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI Calculator App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMI Calculator App
+//
+//  Created by Felipe Alves Amodio on 02/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
